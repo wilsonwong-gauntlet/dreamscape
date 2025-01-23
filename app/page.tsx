fdad4b2ffcc353454d2a6bdb0cedd7539fc56cea
Wilson Wong <wilson.wong@gauntletai.com>
2025-01-22 22:01:27 -0500

    await server 2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
 
 export default async function Home() {
-  const supabase = createClient()
+  const supabase = await createClient()
 
   const { data: { user } } = await supabase.auth.getUser()
 
