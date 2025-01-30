7a44bb3a0749cdf17f507728d0dbc495bc83e63b
Wilson Wong <wilson.wong@gauntletai.com>
2025-01-30 15:43:32 -0500

    investorsync

--- components/layout/nav/AdminNav.tsx
+++ components/layout/nav/AdminNav.tsx
@@ -112,7 +112,7 @@ export default function AdminNav() {
       <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-background px-6 pb-4">
         <div className="flex h-16 shrink-0 items-center">
           <Link href="/admin/dashboard" className="flex items-center space-x-2">
-            <span className="font-bold text-xl">Dreamscape Capital</span>
+            <span className="font-bold text-xl">InvestorSync</span>
           </Link>
         </div>
         <nav className="flex flex-1 flex-col">
--- components/layout/nav/CustomerNav.tsx
+++ components/layout/nav/CustomerNav.tsx
@@ -26,7 +26,7 @@ export default function ClientNav() {
       <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-background px-6 pb-4">
         <div className="flex h-16 shrink-0 items-center">
           <Link href="/portfolio" className="flex items-center space-x-2">
-            <span className="font-bold text-xl">Dreamscape Capital</span>
+            <span className="font-bold text-xl">InvestorSync</span>
           </Link>
         </div>
         <nav className="flex flex-1 flex-col">
